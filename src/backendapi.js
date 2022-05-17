98cd7f18ee231d2381dea5f7c69177665fc46c45
Joel Fintan <joelfintan@gmail.com>
2022-05-17 09:22:51 +0530

    Erase everything.

--- src/backendapi.js
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios'
-const host = {
-    heroku: "//pldds-beta.herokuapp.com",
-    localhost: "//192.168.43.59:5000"
-}
-export default Axios.create({
-    baseURL:host.heroku
-})
